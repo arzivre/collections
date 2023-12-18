f69d9e9bd4c8c58ae418b6841f674761b6eb4096
Arzivre <sonyfauzi@outlook.co.id>
2023-12-18 08:53:17 +0700

    update timer

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -2,40 +2,65 @@
 
 import React, { useState, useEffect } from "react";
 
-const Timer = () => {
-  const [seconds, setSeconds] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
+const Timer: React.FC = () => {
+  const [seconds, setSeconds] = useState<number>(0);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+  const [history, setHistory] = useState<number[]>([]);
 
   useEffect(() => {
     let timerId: NodeJS.Timeout;
 
     const tick = () => {
-      if (!isPaused) {
+      if (!isPaused && isRunning) {
         setSeconds((prevSeconds) => prevSeconds + 1);
       }
     };
 
-    // eslint-disable-next-line prefer-const
-    timerId = setInterval(tick, 1000);
+    if (isRunning) {
+      timerId = setInterval(tick, 1000);
+    }
 
     return () => clearInterval(timerId);
-  }, [isPaused]);
+  }, [isPaused, isRunning]);
+
+  useEffect(() => {
+    // Save history to localStorage
+    localStorage.setItem("timerHistory", JSON.stringify(history));
+  }, [history]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
 
   const handlePauseToggle = () => {
     setIsPaused((prevIsPaused) => !prevIsPaused);
   };
 
   const handleReset = () => {
+    setHistory([...history, seconds]);
     setSeconds(0);
+    setIsRunning(false);
+    setIsPaused(false);
   };
 
   return (
-    <div>
+    <div className="mx-auto flex min-h-screen w-full max-w-5xl flex-col gap-4">
       <p>Timer: {seconds} seconds</p>
-      <button onClick={handlePauseToggle}>
-        {isPaused ? "Resume" : "Pause"}
-      </button>
+      {!isRunning && <button onClick={handleStart}>Start</button>}
+      {isRunning && (
+        <button onClick={handlePauseToggle}>
+          {isPaused ? "Resume" : "Pause"}
+        </button>
+      )}
       <button onClick={handleReset}>Reset</button>
+
+      <h2>Timer History:</h2>
+      <ul>
+        {history.map((time, index) => (
+          <li key={index}>{time} seconds</li>
+        ))}
+      </ul>
     </div>
   );
 };
