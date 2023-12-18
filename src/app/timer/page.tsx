07895f97b3e55f87134e458ce6b35b79b217e822
Arzivre <sonyfauzi@outlook.co.id>
2023-12-18 09:21:41 +0700

    update timer

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Timer: React.FC = () => {
   const [seconds, setSeconds] = useState<number>(0);
   const [isPaused, setIsPaused] = useState<boolean>(false);
   const [isRunning, setIsRunning] = useState<boolean>(false);
-  const [history, setHistory] = useState<number[]>([]);
+  const [history, setHistory] = useState<{ date: string; time: number }[]>([]);
 
   useEffect(() => {
     let timerId: NodeJS.Timeout;
@@ -26,39 +26,58 @@ const Timer: React.FC = () => {
 
   useEffect(() => {
     // Save history to localStorage
-    localStorage.setItem("timerHistory", JSON.stringify(history));
+    localStorage.setItem('timerHistory', JSON.stringify(history));
   }, [history]);
+
+  const getCurrentDate = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, '0');
+    const day = `${now.getDate()}`.padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  };
 
   const handleStart = () => {
     setIsRunning(true);
   };
 
   const handlePauseToggle = () => {
-    setIsPaused((prevIsPaused) => !prevIsPaused);
+    if (isRunning) {
+      setIsPaused((prevIsPaused) => !prevIsPaused);
+      if (isPaused) {
+        const currentDate = getCurrentDate();
+        setHistory((prevHistory) => [
+          ...prevHistory,
+          { date: currentDate, time: seconds },
+        ]);
+      }
+    }
   };
 
   const handleReset = () => {
-    setHistory([...history, seconds]);
+    const currentDate = getCurrentDate();
+    setHistory((prevHistory) => [
+      ...prevHistory,
+      { date: currentDate, time: seconds },
+    ]);
     setSeconds(0);
     setIsRunning(false);
     setIsPaused(false);
   };
 
   return (
-    <div className="mx-auto flex min-h-screen w-full max-w-5xl flex-col gap-4">
+    <div>
       <p>Timer: {seconds} seconds</p>
       {!isRunning && <button onClick={handleStart}>Start</button>}
-      {isRunning && (
-        <button onClick={handlePauseToggle}>
-          {isPaused ? "Resume" : "Pause"}
-        </button>
-      )}
+      {isRunning && <button onClick={handlePauseToggle}>{isPaused ? 'Resume' : 'Pause'}</button>}
       <button onClick={handleReset}>Reset</button>
 
       <h2>Timer History:</h2>
       <ul>
-        {history.map((time, index) => (
-          <li key={index}>{time} seconds</li>
+        {history.map((entry, index) => (
+          <li key={index}>
+            {entry.date} - {entry.time} seconds
+          </li>
         ))}
       </ul>
     </div>
